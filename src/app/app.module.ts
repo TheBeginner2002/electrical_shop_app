331d3cbededa3ee4085079d96aeb0192a1e8e78e
TheBeginer2002 <quangphamxuan61@gmail.com>
2023-06-07 21:19:28 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
@@ -21,7 +21,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  activeSlideIndex = 0;
+  images = [
+    'https://via.placeholder.com/650x500?text=Slide+1',
+    'https://via.placeholder.com/650x500?text=Slide+2',
+    'https://via.placeholder.com/650x500?text=Slide+3'
+  ];
+
+  moveNext() {
+    if(this.activeSlideIndex == this.images.length - 1) {
+      this.activeSlideIndex = 0;
+      return;
+    }
+    this.activeSlideIndex ++;
+  }
+
+  movePrev() {
+    if(this.activeSlideIndex == 0) {
+      this.activeSlideIndex = this.images.length - 1;
+      return;
+    }
+    this.activeSlideIndex --;
+  }
 }
