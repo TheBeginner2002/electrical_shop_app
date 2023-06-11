19bddca6faaa00bd8a9ae0c4a12d80e5682264f2
TheBeginer2002 <quangphamxuan61@gmail.com>
2023-06-11 17:23:33 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component : MainPageComponent },
+  { path: 'product', component : ProductPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { MatButtonModule} from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ImageSliderModule } from './components/image-slider/image-slider.module';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
