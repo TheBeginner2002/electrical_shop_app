491ece7f984a3788a88802877e25e1bc80dc2ade
TheBeginer2002 <quangphamxuan61@gmail.com>
2023-06-14 00:04:31 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule} from '@angular/material/button';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ImageSliderModule } from './components/image-slider/image-slider.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
 import { ToolbarModule } from './components/toolbar/toolbar.module';
-import { MatCardModule } from '@angular/material/card';
 import { CardModule } from './components/card/card.module';
 
 
@@ -27,16 +20,8 @@ import { CardModule } from './components/card/card.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    FontAwesomeModule,
-    MatFormFieldModule,
-    BrowserModule,
-    BrowserAnimationsModule,
     ImageSliderModule,
     ToolbarModule,
-    MatCardModule,
     CardModule
   ],
   providers: [],
