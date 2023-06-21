276fc28c1c559c98ba0310a0d9d593c214b888fa
TheBeginer2002 <quangphamxuan61@gmail.com>
2023-06-21 14:30:43 +0700

    update

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -6,14 +6,14 @@ import { ProductModel } from '../model/product-model';
 })
 export class AppService {
   private cartProducts: ProductModel[] = [];
-  
+
   products: ProductModel[] = [
     {img: '../../../assets/TV1.png', title: 'Super TV', alt: 'TV1', subTitle: '4k Ultra HD 57"', price: 49999000},
-    {img: '../../../assets/TV2.png', title: 'Sakura TV', alt: 'TV1', subTitle: '5k HDR 56"', price: 59999000},
-    {img: '../../../assets/TV3.png', title: 'Google TV', alt: 'TV1', subTitle: '4k Ultra HD 55"', price: 67999000},
-    {img: '../../../assets/TV1.png', title: 'Super TV', alt: 'TV1', subTitle: '4k Ultra HD 57"', price: 49999000},
-    {img: '../../../assets/TV2.png', title: 'Sakura TV', alt: 'TV1', subTitle: '5k HDR 56"', price: 59999000},
-    {img: '../../../assets/TV3.png', title: 'Google TV', alt: 'TV1', subTitle: '4k Ultra HD 55"', price: 67999000},
+    {img: '../../../assets/TV2.png', title: 'Sakura TV', alt: 'TV2', subTitle: '5k HDR 56"', price: 59999000},
+    {img: '../../../assets/TV3.png', title: 'Google TV', alt: 'TV3', subTitle: '4k Ultra HD 55"', price: 67999000},
+    {img: '../../../assets/TV4.png', title: 'LG TV', alt: 'TV4', subTitle: 'Full HD 57"', price: 4999000},
+    {img: '../../../assets/TV5.png', title: 'China TV', alt: 'TV5', subTitle: '4k HDR 60"', price: 79999000},
+    {img: '../../../assets/TV6.png', title: 'Samsung TV', alt: 'TV6', subTitle: '4k Ultra HD 50"', price: 69999000},
   ];
 
   constructor() {
@@ -33,7 +33,7 @@ export class AppService {
 
   addCart(product: ProductModel): void {
     this.cartProducts.push(product);
-    localStorage.setItem('cartProducts', JSON.stringify(this.cartProducts)); 
+    localStorage.setItem('cartProducts', JSON.stringify(this.cartProducts));
   }
 
   clearCart() {
